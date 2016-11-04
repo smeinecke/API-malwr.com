d581a1ac8784e7be8e6ff05abad4bdded5e63178
Hannah Ward <Hannah.ward9001@gmail.com>
2016-11-04 13:04:54 +0000

    Added a setup.py and restructured

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="Malwr API",
+    description="A basic API to Malwr",
+    version="0.1",
+    author="PaulSec",
+    url="https://github.com/PaulSec/API-malwr.com",
+    packages=["malwr"]
+)
